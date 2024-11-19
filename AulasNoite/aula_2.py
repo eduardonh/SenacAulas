95635d84957346848e73205cb865d27b9813ad21
José Eduardo Soares de Lima <61420032+eduardonh@users.noreply.github.com>
2024-11-19 02:19:08 +0000

    Exercicios de lógica.

---
+++ AulasNoite/aula_2.py
@@ -0,0 +1,12 @@
+faturamento = 1200
+custo = 700
+lucro = faturamento - custo
+margem_lucro = lucro / faturamento
+
+print(f"Faturamento da empresa: R$ {faturamento:.2f}  Custo: R$ {custo:.2f}  Lucro: R$  {lucro:.2f}")
+
+email_cliente = "qualquercoisaaletoria@gmail.com"
+
+# Maiusculas
+email_cliente = email_cliente.upper()
+print(email_cliente)
