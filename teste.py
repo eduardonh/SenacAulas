2c89c1dc3225546e8c691ba6801a770b52cdd0c3
José Eduardo Soares de Lima <61420032+eduardonh@users.noreply.github.com>
2024-11-22 14:42:00 +0000

    Gravar arquivos

---
+++ teste.py
@@ -0,0 +1,19 @@
+idade = 20
+if idade >= 18:
+    print("Você é maior de idade.")
+elif idade < 18 and idade > 12:
+    print("Você pe adolecente.")
+else:
+    print("Você é criança.")
+
+
+for i in range(5):
+    print(i)
+
+
+contador = 0
+while contador < 5:
+    print(contador)
+    contador += 1
+
+
