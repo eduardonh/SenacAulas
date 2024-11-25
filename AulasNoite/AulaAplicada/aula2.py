790b74154dde241307ac46e43e2f7752d41b279e
José Eduardo Soares de Lima <61420032+eduardonh@users.noreply.github.com>
2024-11-25 14:59:18 +0000

    Gravar

---
+++ AulasNoite/AulaAplicada/aula2.py
@@ -0,0 +1,10 @@
+faturamento = 1200 # Tipo: int -> número inteiro.
+custo = 750.32 # Tipo: float -> número com casas decimais.
+lucro = faturamento - custo
+
+margemLucro = lucro/faturamento
+
+print("Faturamento foi de ", faturamento)
+print("O custo foi de ", custo)
+print("O lucro foi de ", lucro)
+print("A margem de lucro foi de ", margemLucro)
