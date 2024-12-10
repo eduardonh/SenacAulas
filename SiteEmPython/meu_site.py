d591c763ae38ce060b7717394938595ac366adf7
José Eduardo Soares de Lima <61420032+eduardonh@users.noreply.github.com>
2024-12-10 19:55:33 +0000

    git add .

--- SiteEmPython/meu_site.py
+++ SiteEmPython/meu_site.py
@@ -9,6 +9,10 @@ app = Flask(__name__)
 def homepage():
     return "Esse é meu primeiro site."
 
+@app.route("/contatos")
+def contatos():
+    return "<p>Contatos do Site: </p><p>E-mail: eduardo.nh1969@gmail.com</p><p> Fone: (051)9999.9999</p>"
+
 # colocar o site no ar
 if __name__ == "__main__":
     app.run(debug=True)
