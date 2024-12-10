511826de67128c4386074427a461a456b5b34c9e
José Eduardo Soares de Lima <61420032+eduardonh@users.noreply.github.com>
2024-12-10 19:13:09 +0000

    Gravação arquivos

--- SiteEmPython/meu_site.py
+++ SiteEmPython/meu_site.py
@@ -7,15 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def homepage():
-    return render_template("homepage.html")
-
-@app.route("/contatos")
-def contatos():
-    return render_template("contatos.html")
-
-@app.route("/usuarios/<nome_usuario>")
-def usuarios(nome_usuario):
-    return render_template("usuarios.html", nome_usuario=nome_usuario)
+    return "Esse é meu primeiro site."
 
 # colocar o site no ar
 if __name__ == "__main__":
